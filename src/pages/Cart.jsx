d31840ba0afbeb1c448b577308b4f86f9b62fc5e
stringerc <stringer.c.a@gmail.com>
2024-03-27 23:19:49 -0400

    FIrst Initial Commit

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -183,7 +183,7 @@ const Cart = () => {
                     <SummaryTitle>ORDER SUMMARY</SummaryTitle>
                     <SummaryItem>
                         <SummaryItemText>Subtotal</SummaryItemText>
-                        <SummaryItemPrice>$80</SummaryItemPrice>
+                        <SummaryItemPrice>$100</SummaryItemPrice>
                     </SummaryItem>
                     <SummaryItem>
               <SummaryItemText>Estimated Shipping</SummaryItemText>
@@ -195,7 +195,7 @@ const Cart = () => {
             </SummaryItem>
             <SummaryItem type="total">
               <SummaryItemText>Total</SummaryItemText>
-              <SummaryItemPrice>$ 80</SummaryItemPrice>
+              <SummaryItemPrice>$ 100</SummaryItemPrice>
             </SummaryItem>
             <Button>CHECKOUT NOW</Button>
                 </Summary>
